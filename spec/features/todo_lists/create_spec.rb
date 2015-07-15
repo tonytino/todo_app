c59975e5f82efda0a1eca5b9ee03bec15c3db3a8
Tonytino <tony91139@Gmail.com>
2015-07-14 19:53:51 -0700

    Add length and presence validation to todo list title with tests

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe 'Creating todo lists' do
+    it 'redirects to the todo list index page on success' do
+        visit '/todo_lists'
+        click_link 'New Todo list'
+        expect(page).to have_content("New Todo List")
+
+        fill_in "Title", with: "My todo list"
+        fill_in "Description", with: "This is what I'm doing today."
+        click_button "Create Todo list"
+
+        expect(page).to have_content("My todo list")
+    end
+
+    it 'displays an error when the todo list has no title' do
+        # we expect that the database is cleared after each test
+        expect(TodoList.count).to eq(0)
+
+        visit '/todo_lists'
+        click_link 'New Todo list'
+        expect(page).to have_content("New Todo List")
+
+        fill_in "Title", with: "" # notice that we purposely didn't add a title
+        fill_in "Description", with: "This is what I'm doing today."
+        click_button "Create Todo list"
+
+        # expect that we still don't have any todo lists in our db
+        expect(TodoList.count).to eq(0)
+
+        expect(page).to have_content("error")
+
+        visit '/todo_lists'
+        expect(page).to_not have_content("This is what I'm doing today.")
+    end
+
+    it 'displays an error when the todo list has a title less than 3 characters' do
+        # we expect that the database is cleared after each test
+        expect(TodoList.count).to eq(0)
+
+        visit '/todo_lists'
+        click_link 'New Todo list'
+        expect(page).to have_content("New Todo List")
+
+        fill_in "Title", with: "Hi"
+        fill_in "Description", with: "This is what I'm doing today."
+        click_button "Create Todo list"
+
+        # expect that we still don't have any todo lists in our db
+        expect(TodoList.count).to eq(0)
+
+        expect(page).to have_content("error")
+
+        visit '/todo_lists'
+        expect(page).to_not have_content("This is what I'm doing today.")
+    end
+end
