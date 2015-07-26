fd366fbb9fb96498642bd58dd7f55eccbfd2aebb
Tonytino <tony91139@Gmail.com>
2015-07-26 13:21:01 -0700

    Add test for todo item creation error: no content

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -19,10 +19,27 @@ describe 'Adding todo items' do
     fill_in 'Content', with: 'Milk'
     click_button 'Save'
 
-    expect(page).to have_content('Added todo list item.')
+    within('div.flash') do
+      expect(page).to have_content('Added todo list item.')
+    end
+
     within('ul.todo_items') do
       expect(page).to have_content('Milk')
     end
+  end
+
+  it 'displays an error with no content' do
+    visit_todo_list(todo_list)
+
+    click_link 'New Todo Item'
+    fill_in 'Content', with: ''
+    click_button 'Save'
+
+    within('div.flash') do
+      expect(page).to have_content('There was a problem adding that todo list item.')
+    end
+
+    expect(page).to have_content("Content can't be blank")
   end
 
 end
