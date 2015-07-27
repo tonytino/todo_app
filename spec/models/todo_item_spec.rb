fd3d4e6bb5c4d3adc17c2a31c64800ef4c074fd9
Tonytino <tony91139@Gmail.com>
2015-07-27 13:48:31 -0700

    Reduce logic in view regarding mark item complete
    Add #completed? method to view for TodoItem to check if item is completed (returns true/false)
    Implemented if/else in view using #completed? to add time of completion or option to mark item complete
    Add two model tests regarding #completed? method:
    - returns false when completed_at is blank/nil
    - returns ture when completed_at is not blank/nil

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -4,4 +4,19 @@ describe TodoItem do
 
   it { should belong_to(:todo_list) }
 
+  describe '#completed?' do
+    let(:todo_item) { TodoItem.create(content: 'Finish this test') }
+
+    it 'is false when completed_at is blank' do
+      todo_item.completed_at = nil
+
+      expect(todo_item.completed?).to be false
+    end
+
+    it 'is true when completed_at is not blank' do
+      todo_item.completed_at = Time.now
+
+      expect(todo_item.completed?).to be true
+    end
+  end
 end
