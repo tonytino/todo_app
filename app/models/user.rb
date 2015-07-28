451a582e010b5062134257363ec969aab803af49
Tonytino <tony91139@Gmail.com>
2015-07-28 13:23:35 -0700

    Add validations and #downcase_email to user model w/ tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+
+  validates :email, presence: true,
+                    uniqueness: true
+
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase!
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:valid_attributes) {
+    {
+      first_name: 'Anthony',
+      last_name: 'Hernandez',
+      email: "anthony@mail.com"
+    }
+  }
+
+  context 'validations' do
+    # Note that this user is not saved
+    let(:user) { User.new(valid_attributes) }
+
+    # We create a user in the database first to make sure valid attributes are indeed valid (i.e. aren't rejected)
+    before do
+      User.create(valid_attributes)
+    end
+
+    it 'requires an email' do
+      expect(user).to validate_presence_of(:email)
+    end
+
+    it 'requires a unique email' do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+  end
+
+  context '#downcase_email' do
+    it 'makes the email attribute lower case' do
+      user = User.new(valid_attributes.merge(email: "ANTHONY@MAIL.COM"))
+      user.downcase_email
+      expect(user.email).to eq('anthony@mail.com')
+    end
+
+    it 'downcases an email before saving' do
+      user = User.new(valid_attributes)
+      user.email = "ANTHONY@MAIL.COM"
+      expect(user.save).to be true
+      expect(user.email).to eq('anthony@mail.com')
+    end
+  end
 end
