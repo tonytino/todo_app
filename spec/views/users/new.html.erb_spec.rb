4ee2e1dfd4e76d46a9679059ed896c46c1717536
Tonytino <tony91139@Gmail.com>
2015-07-29 11:35:40 -0700

    Remove password_digest from failing specs to remedy

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -6,7 +6,7 @@ describe "users/new" do
       :first_name => "MyString",
       :last_name => "MyString",
       :email => "MyString",
-      :password_digest => "MyString"
+      :password => "MyString"
     ).as_new_record)
   end
 
@@ -18,7 +18,7 @@ describe "users/new" do
       assert_select "input#user_first_name[name=?]", "user[first_name]"
       assert_select "input#user_last_name[name=?]", "user[last_name]"
       assert_select "input#user_email[name=?]", "user[email]"
-      assert_select "input#user_password_digest[name=?]", "user[password_digest]"
+      assert_select "input#user_password[name=?]", "user[password]"
     end
   end
 end
