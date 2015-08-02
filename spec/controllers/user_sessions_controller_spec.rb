1827d3f464ea4448a5a5cbc2324886ff80df52aa
Tonytino <tony91139@Gmail.com>
2015-08-01 22:46:30 -0700

    Add success tests for post create user_sesh contrlr, some deprec errors

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -15,7 +15,26 @@ describe UserSessionsController do
   end
 
   describe "POST 'create'" do
+    context 'with correct credentials' do
+      let!(:user) { User.create(first_name: 'Anthony', last_name: 'Hernandez', email: 'anthony@mail.com', password: 'monkey123', password_confirmation: 'monkey123') }
 
+      it 'redirects to the todo list path' do
+        post :create, email: 'anthony@mail.com', password: 'monkey123'
+        expect(response).to be_redirect
+        expect(response).to redirect_to(todo_lists_path)
+      end
+
+      it 'finds the user' do
+        expect(User).to receive(:find_by).with({ email: 'anthony@mail.com' }).and_return(user)
+        post :create, email: 'anthony@mail.com', password: 'monkey123'
+      end
+
+      it 'authenticates the user' do
+        User.stub(:find_by).and_return(user)
+        expect(user).to receive(:authenticate)
+        post :create, email: 'anthony@mail.com', password: 'monkey123'
+      end
+    end
   end
 
 end
